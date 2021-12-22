0d20f7526bc6d724f48ab73875fda33c3bbeaff1
Akinmyrza Kanat uulu <akinmyrza@mancho.dev>
2021-12-22 14:31:58 +0600

    added widgets

---
+++ lib/screens/location_detail/location_detail.dart
@@ -0,0 +1,25 @@
+//locationDetail.dart
+import 'package:flutter/material.dart';
+import 'image_banner.dart';
+import 'text_section.dart';
+
+class LocationDetail extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Location Detail'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ImageBanner("assets/images/Mountain.jpeg"),
+          TextSection("TextTitle1", "Something 1"),
+          TextSection("TextTitle2", "Something 2"),
+          TextSection("TextTitle3", "Something 3"),
+        ],
+      ),
+    );
+  }
+}
